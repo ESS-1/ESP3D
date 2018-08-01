5439f0ca991ce3ab61d519e5364d3d73b2da31ad
Eugene Shelkovin <eugene.shelkovin@gmail.com>
2018-08-02 02:46:10 +0300

    Version changed 1.0 => 1.1

--- esp3d/config.h
+++ esp3d/config.h
@@ -183,7 +183,7 @@ extern "C" {
 #endif
 #include "wificonf.h"
 //version and sources location
-#define FW_VERSION "1.0"
+#define FW_VERSION "1.1"
 #define REPOSITORY "https://github.com/luc-github/ESP3D"
 
 typedef enum {
